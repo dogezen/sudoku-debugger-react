1da10e5415d4ce81ab128d182fa751b456f68cc5
Carlo M <carlo992@gmail.com>
2024-11-09 11:54:15 -0600

    Added mantine with dark theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createTheme, MantineProvider } from "@mantine/core";
 
-createRoot(document.getElementById('root')!).render(
+import App from "./App.tsx";
+
+import "@mantine/core/styles.css";
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <MantineProvider theme={theme} forceColorScheme="dark">
+      <App />
+    </MantineProvider>
   </StrictMode>,
-)
+);
